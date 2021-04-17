16f3661451d1f587996e84646457fd85276412bd
npcxh <bakanpcxh@gmail.com>
2021-04-17 16:21:09 +0800

    基本完成搜索界面

--- src/views/My.vue
+++ src/views/My.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>My</div>
+  <el-card :body-style="{ display: 'flex' }">
+    <template #header>当前借阅</template>
+    <div class="borrow">
+      <div class="borrow_num"> 0 </div>
+      <div class="borrow_text">已借阅</div>
+    </div>
+    <el-divider direction="vertical" class="divider"></el-divider>
+     <div class="expire">
+      <div class="expire_num"> 0 </div>
+      <div class="expire_text">已超期</div>
+    </div>
+  </el-card>
 </template>
 
 <script>
@@ -8,6 +19,23 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.borrow, .expire {
+  width: 50%;
+  text-align: center;
 
+  &_num {
+    font-size: 24px;
+    margin-bottom: 16px;
+    color: #409EFF;
+  }
+}
+
+.expire_num {
+  color: #F56C6C;
+}
+
+.divider {
+  height: auto;
+}
 </style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,13 +1,118 @@
 <template>
-  <div>Search</div>
+  <el-card :body-style="{ 'text-align': 'center'}" class="card">
+    <template #header>书籍查询</template>
+    <div class="search">
+      <el-input
+       prefix-icon="el-icon-search"
+       style="width: 20%" v-model="keyWord"
+       placeholder="书名模糊搜索"></el-input>
+      <el-button type="primary" class="search_button" @click="searchBook">搜索</el-button>
+    </div>
+  </el-card>
+  <el-card shadow="hover" class="card" v-for="item in bookList" :key="item.bid">
+    <div class="result">
+      <el-image class="result_img" :src="'http://lib.cqu.edu.cn/images/default-cover5.png'"></el-image>
+      <div class="result_info">
+        <span class="result_info_header">{{item.bname}}</span>
+        <span class="result_info_title">作者: <a class="result_info_text"> {{item.author}}</a></span>
+        <span class="result_info_title">出版日期: <a class="result_info_text"> {{item.Bdate}}</a></span>
+        <span class="result_info_title">馆藏余量: <a class="result_info_text"> {{item.amount}}</a></span>
+      </div>
+      <el-button type="primary" class="result_button" @click="borrowBook(item.bid)">借书</el-button>
+   </div>
+  </el-card>
 </template>
 
 <script>
+import { ref } from 'vue'
+import { get, post } from '../utils/request'
+import { ElMessage } from 'element-plus'
+
+const useSearchEffect = () => {
+  const bookList = ref({})
+  const keyWord = ref('')
+  const searchBook = async () => {
+    try {
+      const result = await get('/books', { name: keyWord.value })
+      if (result?.code === 200) {
+        bookList.value = result.data
+        console.log(bookList)
+      } else {
+        ElMessage.error('搜索失败')
+      }
+    } catch (e) {
+      ElMessage.error(e)
+    }
+  }
+  return { keyWord, searchBook, bookList }
+}
+
+const useBorrowEffect = () => {
+  const borrowBook = async (bid) => {
+    try {
+      const result = await post('/borrow', { uid: localStorage.uid, bid: bid })
+      if (result?.code === 200) {
+        ElMessage.success(result.msg)
+      } else {
+        ElMessage.error(result.msg)
+      }
+    } catch (e) {
+      ElMessage.error(e)
+    }
+  }
+
+  return borrowBook
+}
+
 export default {
-  name: 'Search'
+  name: 'Search',
+  setup () {
+    const { keyWord, searchBook, bookList } = useSearchEffect()
+    const borrowBook = useBorrowEffect()
+    return { keyWord, searchBook, bookList, borrowBook }
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+.card {
+  margin-bottom: 20px;
+}
+
+.search {
+  &_button {
+    margin-left: 12px;
+  }
+}
+
+.result{
+  display: flex;
+  align-items: center;
+  margin: 16px 24px;
+  &_info {
+    text-align: left;
+    margin-left: 24px;
+    &_header {
+      font-size: 20px;
+      display: block;
+      font-weight: bold;
+      margin-bottom: 16px;
+    }
+    &_title {
+      display: block;
+      margin-bottom: 12px;
+      font-weight: bold;
+    }
+    &_text {
+      color: #606266;
+      font-weight: normal;
+    }
+  }
+  &_button {
+    margin-left: auto;
+    height: 10%;
+  }
+}
 
 </style>
